33dbdef545fdc5944b66134b2474132ce2f5a206
Yak <yassinekhayri123@gmail.com>
2024-08-14 14:52:34 +0100

    Ex02 Done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -8,6 +8,9 @@
 template <class T> class MutantStack : public std::stack<T>{
 	public:
 		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 		iterator begin(){ return this->c.begin();}
 		iterator end(){ return this->c.end();}
+		reverse_iterator rbegin(){ return this->c.rbegin();}
+		reverse_iterator rend(){ return this->c.rend();}
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -11,16 +11,23 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
 	++it;
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "*************** rev *****************" << std::endl;
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
 	}
 	std::stack<int> s(mstack);
 	return 0;
