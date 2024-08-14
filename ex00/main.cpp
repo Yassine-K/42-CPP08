3c2b94229cdee9cbf03ff972b6128180a3b304bb
Yak <yassinekhayri123@gmail.com>
2024-08-14 15:24:52 +0100

    Ex00 Done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 10:11:11 by ykhayri           #+#    #+#             */
-/*   Updated: 2024/08/12 10:55:46 by ykhayri          ###   ########.fr       */
+/*   Updated: 2024/08/14 15:23:43 by ykhayri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 int main() {
 	std::vector<int> a;
 	std::list<int> b;
-	std::array<int, 5> c = {15,3,9,11,2};
+	std::array<int, 10> c;
 	std::deque<int> d;
 
 	for (int i = 0; i < 10; i++)
 		a.push_back(i + 1);
 	for (int i = 0; i < 10; i++)
 		b.push_back(i + 2);
+	for (int i = 0; i < 10; i++)
+		c[i] = i + 4;
 	for (int i = 0; i < 10; i++)
 		d.push_back(i + 3);
 
